86d42ef032c4d1161e66689c8bb55243db8c9887
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-07 01:32:50 -0800

    social icons, main sass file for global

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,40 @@
 import ContactForm from "../components/ContactForm"
+import styles from '../styles/Contact.module.css';
+
 
 const Contact = () => {
    
     return(
-        <div>
-        <h1>Get in touch!</h1>
+        <div className={styles.contactContainer}>
+            <div className={styles.contactInfo}>
+                <h3>Get in touch!</h3>
+                <div className={styles.socialMedia}>
+                    <a href="#" className={styles.icon}>
+                    <i class="fa fa-instagram-square" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div className={styles.socialMedia}>
+                    <a href="#" className={styles.icon}>
+                        <i class="fa fa-medium" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div className={styles.socialMedia}>
+                    <a href="#" className={styles.icon}>
+                        <i class="fa fa-github-square" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div className={styles.socialMedia}>
+                    <a href="#" className={styles.icon}>
+                        <i class="fa fa-linkedin" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div className={styles.socialMedia}>
+                    <a href="#" className={styles.icon}>
+                        <i class="fa fa-twitch" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+        
         
         <ContactForm/>
         </div>
