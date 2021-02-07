3c5badc6816a346ee60762d750f3eeb512db0552
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-07 12:15:14 -0800

    contact styling, deleted sass file

--- pages/contact.js
+++ pages/contact.js
@@ -5,39 +5,34 @@ import styles from '../styles/Contact.module.css';
 const Contact = () => {
    
     return(
-        <div className={styles.contactContainer}>
-            <div className={styles.contactInfo}>
-                <h3>Get in touch!</h3>
-                <div className={styles.socialMedia}>
-                    <a href="#" className={styles.icon}>
-                    <i class="fa fa-instagram-square" aria-hidden="true"></i>
-                    </a>
+        <main className={styles.main}>
+            <h1>Get in touch!</h1>
+            <div className={styles.contactContainer}>
+                <div className={styles.contactInfo}>
+                    <h3 className={styles.h3}>Sam Kuttenkuler</h3>
+                    <div className={styles.socialMedia}>
+                        <a href="#" className={styles.icon}>
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                        <a href="#" className={styles.icon}>
+                            <i class="fa fa-medium" aria-hidden="true"></i>
+                        </a>
+                        <a href="#" className={styles.icon}>
+                            <i class="fa fa-github-square" aria-hidden="true"></i>
+                        </a>
+                        <a href="#" className={styles.icon}>
+                            <i class="fa fa-linkedin" aria-hidden="true"></i>
+                        </a>
+                        <a href="#" className={styles.icon}>
+                            <i class="fa fa-twitch" aria-hidden="true"></i>
+                        </a>
+                    </div>
                 </div>
-                <div className={styles.socialMedia}>
-                    <a href="#" className={styles.icon}>
-                        <i class="fa fa-medium" aria-hidden="true"></i>
-                    </a>
-                </div>
-                <div className={styles.socialMedia}>
-                    <a href="#" className={styles.icon}>
-                        <i class="fa fa-github-square" aria-hidden="true"></i>
-                    </a>
-                </div>
-                <div className={styles.socialMedia}>
-                    <a href="#" className={styles.icon}>
-                        <i class="fa fa-linkedin" aria-hidden="true"></i>
-                    </a>
-                </div>
-                <div className={styles.socialMedia}>
-                    <a href="#" className={styles.icon}>
-                        <i class="fa fa-twitch" aria-hidden="true"></i>
-                    </a>
-                </div>
+        
+        
+            <ContactForm/>
             </div>
-        
-        
-        <ContactForm/>
-        </div>
+        </main>
         )
 };
 
