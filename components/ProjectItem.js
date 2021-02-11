dbaf03368bc7c07061d8280be495fec1c544e339
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-10 17:50:15 -0800

    restructured project data, maping project tech

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -1,18 +1,40 @@
+import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-
+import {SiReact,
+    SiTypescript,
+    SiRedux,
+    FaNodeJs,
+    SiFirebase,
+    SiDjango,
+    SiHtml5,
+    SiCss3,
+    SiJavascript,
+    SiUnity,
+    SiMongodb,} from 'react-icons/si';
 import styles from '../styles/ProjectsList.module.css'
 
 const ProjectItem = ({project, object_key}) => {
+
     return (
         <Link href={project.deployed} passHref={true}>
             <a className={styles.card}>
                 <h3 key={object_key}>{project.title} &rarr;</h3>
+                <div className={styles.projectContainer}>
+                   <div>
+                   {project.tech.map((t, i) => ( 
+                    <div key={i}>
+                        {React.createElement(t.icon)}
+                        
+                    </div>
+                    ))}
+                    </div>
                 <Image src={project.thumbnail}
                   alt="project image"
                   height={500}
                   width={500}
                     />
+                </div>
             </a>
         </Link>
     )
--- projectdata.js
+++ projectdata.js
@@ -4,7 +4,29 @@ export const projects = [
         title: 'Falcon Heavy',
         deployed: '//immense-beyond-39985.herokuapp.com',
         code: '//www.github.com/skuttenkuler/SpaceX',
-        thumbnail: '/images/FalconHeavey.png',
+        thumbnail: '/images/projects/FalconHeavey.png',
+        tech: [
+            {
+                name:"React",
+                icon:"<SiReact/>"
+            },
+            {
+                name:"TypeScript",
+                icon:"<SiTypescript/>"
+            },
+            {
+                name:"Redux",
+                icon:"<SiRedux/>"
+            },
+            {
+                name:"Node",
+                icon:"<FaNodeJs/>"
+            },
+            {
+                name:"Firebase",
+                icon:"<SiFirebase/>"
+            }
+        ],
         description: "Learn the differences of the 4 main versions of the Falcon Rocket developed by SpaceX."
     },
     {
@@ -12,7 +34,26 @@ export const projects = [
         title: 'Star Social',
         deployed: '//samkuttenk2.pythonanywhere.com/',
         code: '//www.github.com/skuttenkuler/StarSocial/',
-        thumbnail: '/images/StarSocial.png',
+        thumbnail: '/images/projects/StarSocial.png',
+        tech: [
+            {
+                name:"Django",
+                icon:"<SiDjango/>"
+            },
+            {
+                name:"HTML",
+                icon:"<SiHtml5/>"
+            },
+            {
+                name:"CSS",
+                icon:"<SiCss3/>"
+            },
+            {
+                name:"Javascript",
+                icon:"<SiJavascript/>"
+            },
+           
+        ],
         description: "Join the social network for all things space related!"
     },
     {
@@ -20,7 +61,16 @@ export const projects = [
         title: 'Hacker Terminal',
         deployed: '//samkuttenk2.pythonanywhere.com/',
         code: '//www.github.com/skuttenkuler/HackerTerminalGame',
-        thumbnail: '/images/HackerTerm.png',
+        thumbnail: '/images/projects/HackerTerm.png',
+        tech: [
+            {
+                name:"Unity",
+                icon:"<SiUnity/>"
+            },
+            {
+                name:"C#",
+            }
+        ],
         description: "A simple C# Unity game. Hack one of three institutes and get a reward for guessing right."
     },
     {
@@ -28,7 +78,13 @@ export const projects = [
         title: 'Gorillaz Game',
         deployed: '//skuttenkuler.github.io/Gorillaz-Clicky-Game/',
         code: '//www.github.com/skuttenkuler/Gorillaz-Clicky-Game',
-        thumbnail: '/images/Gorillaz.jpg',
+        thumbnail: '/images/projects/Gorillaz.jpg',
+        tech: [
+            {
+                name:"react",
+                icon:"<SiReact/>"
+            },
+        ],
         description: "A Gorillaz inspiried Click Game."
     },
     {
@@ -36,7 +92,21 @@ export const projects = [
         title: 'Simpsons Quiz',
         deployed: '//skuttenkuler.github.io/Code-Quiz/',
         code: '//www.github.com/skuttenkuler/Code-Quiz',
-        thumbnail: '/images/Simpsons.png',
+        thumbnail: '/images/projects/Simpsons.png',
+        tech: [
+            {
+                name:"HTML",
+                icon:"<SiHtml5/>"
+            },
+            {
+                name:"CSS",
+                icon:"<SiCss3/>"
+            },
+            {
+                name:"Javascript",
+                icon:"<SiJavascript/>"
+            },
+        ],
         description: "Test your Simpsons knowledge, take the quiz man!"
     },
     {
@@ -44,7 +114,13 @@ export const projects = [
         title: 'Pupper',
         deployed: '//salty-harbor-03873.herokuapp.com/',
         code: '//github.com/skuttenkuler/Pupper',
-        thumbnail: '/images/pupper.jpg',
+        thumbnail: '/images/projects/pupper.jpg',
+        tech: [
+            {
+                name:"React",
+                icon:"<SiReact/>"
+            },
+        ],
         description: "A React application to find and match with the best good boys and girls."
     },
     {
@@ -52,7 +128,21 @@ export const projects = [
         title: 'Book Store',
         deployed: '//still-earth-60188.herokuapp.com/',
         code: '//github.com/skuttenkuler/Book-Store',
-        thumbnail: '/images/books.png',
+        thumbnail: '/images/projects/books.png',
+        tech: [
+            {
+                name:"React",
+                icon:"<SiReact/>"
+            },
+            {
+                name:"Node",
+                icon:"<FaNodeJs/>"
+            },
+            {
+                name:"MongoDB",
+                icon:"<SiMongodb/>"
+            }, 
+        ],
         description: "Search for your favorite books using the Google Books API!"
     },
 ]
