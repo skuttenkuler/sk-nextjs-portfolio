90e2a12af4bfee9d08705b1d212c674d949f0dce
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-15 23:45:42 -0800

    resume mobile

--- pages/contact.js
+++ pages/contact.js
@@ -14,19 +14,19 @@ const Contact = () => {
                 <div className={styles.contactInfo}>
                     <h3 className={styles.name}>Sam Kuttenkuler</h3>
                     <ul id="contact-list"className={styles.socialMedia}>
-                        <a href="#" className={styles.icon}>
+                        <a href="//www.instagram.com/kootkoot/" className={styles.icon}>
                             <i class="fa fa-instagram" aria-hidden="true"></i>
                         </a>
-                        <a href="#" className={styles.icon}>
+                        <a href="//sam-kuttenk.medium.com/" className={styles.icon}>
                             <i class="fa fa-medium" aria-hidden="true"></i>
                         </a>
-                        <a href="#" className={styles.icon}>
+                        <a href="//github.com/skuttenkuler" className={styles.icon}>
                             <i class="fa fa-github-square" aria-hidden="true"></i>
                         </a>
-                        <a href="#" className={styles.icon}>
+                        <a href="//www.linkedin.com/in/skuttenkuler/" className={styles.icon}>
                             <i class="fa fa-linkedin" aria-hidden="true"></i>
                         </a>
-                        <a href="#" className={styles.icon}>
+                        <a href="//www.twitch.tv/kootlicious" className={styles.icon}>
                             <i class="fa fa-twitch" aria-hidden="true"></i>
                         </a>
                     </ul>
--- pages/resume.js
+++ pages/resume.js
@@ -3,27 +3,19 @@ import HomeButton from '../components/HomeButton'
 import styles from '../styles/Resume.module.css'
 
 const Resume = () => {
-    const [resume, setResume] = useState("")
-    const [bcert, setBCert] = useState("")
-    const [scert, setSCert] = useState("")
-    const setResLink = (resume) => {
-        setResume(resume)
-    } 
-    const setBCertLink = (bcert) => {
-        setBCert(bcert)
-    }
-    const setSCertLink = (scert) => {
-        setSCert(scert)
-    }
+   
     
     return(
         <>
         <HomeButton/>
-        <ul className={styles.resCertList}>
+        <div className={styles.res}>
             <div id="resumelink"className={styles.resumeLink}>
                 <h3 className={styles.materialTitle}>Resume.</h3>
             <a className={styles.links} href="/images/Resume_SamKuttenkuler.pdf"download >Download.</a>
             </div>
+        </div>
+        <h2 className={styles.certHeader}>Certificates.</h2>
+        <ul className={styles.certList}>
             <div id="resumelink"className={styles.resumeLink}>
                 <h3 className={styles.materialTitle}>UC Berkeley</h3>
                 <p className={styles.description}>Full Stack Web Developer</p>
