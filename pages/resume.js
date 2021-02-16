2fe66d7e828e99a68264ae1d27e5f32c430b1ade
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-15 23:23:22 -0800

    new resume layout, styles

--- pages/resume.js
+++ pages/resume.js
@@ -1,13 +1,41 @@
-import ResumeList from '../components/ResumeList'
+import React, {useState} from 'react'
 import HomeButton from '../components/HomeButton'
+import styles from '../styles/Resume.module.css'
 
 const Resume = () => {
+    const [resume, setResume] = useState("")
+    const [bcert, setBCert] = useState("")
+    const [scert, setSCert] = useState("")
+    const setResLink = (resume) => {
+        setResume(resume)
+    } 
+    const setBCertLink = (bcert) => {
+        setBCert(bcert)
+    }
+    const setSCertLink = (scert) => {
+        setSCert(scert)
+    }
+    
     return(
         <>
         <HomeButton/>
-        <h1>This is the Resume page</h1>
-        <ResumeList/>
-        </>
+        <ul className={styles.resCertList}>
+            <div id="resumelink"className={styles.resumeLink}>
+                <h3 className={styles.materialTitle}>Resume.</h3>
+            <a className={styles.links} href="/images/Resume_SamKuttenkuler.pdf"download >Download.</a>
+            </div>
+            <div id="resumelink"className={styles.resumeLink}>
+                <h3 className={styles.materialTitle}>UC Berkeley</h3>
+                <p className={styles.description}>Full Stack Web Developer</p>
+            <a className={styles.links} href="/images/Berkley_Certificate.pdf">View.</a>
+            </div>
+            <div id="resumelink"className={styles.resumeLink}>
+                <h3 className={styles.materialTitle}>Stanford University</h3>
+                <p className={styles.description}>Machine Learning</p>
+            <a className={styles.links} href="/images/MLCertificate.pdf">View.</a>
+            </div>
+        </ul>
+         </>
         )
 };
 
