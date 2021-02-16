83c70013d9f2df009c9ba88f84665838d72c54c8
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-16 13:19:05 -0800

    added netlify.toml, changed production server end

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // Target must be serverless
+    target: "serverless",
     images: {
       domains: ['cdn-images-1.medium.com','samkuttenk-resume.s3-us-west-1.amazonaws.com','avatars0.githubusercontent.com'],
     },
