17f808a89b9f97982f1dce495c5721c49b88d9ca
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-16 13:22:48 -0800

    server name fix

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://focused-curran-7f571e.netlify.app/'
+export const server = dev ? 'http://localhost:3000' : 'https://focused-curran-7f571e.netlify.app'
