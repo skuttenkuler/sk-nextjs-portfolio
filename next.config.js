6a4845e86de89ed8057eb339aef5b3a0a1a6b3f9
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-17 14:23:39 -0800

    image amp experiment amp validation

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ module.exports = {
     images: {
       domains: ['cdn-images-1.medium.com','samkuttenk-resume.s3-us-west-1.amazonaws.com','avatars0.githubusercontent.com'],
     },
+    experimental: {
+      amp: {
+        skipValidation: true
+      }
+    },
 
   }
 
