c3ba236d2cb20d30a62b32ddfacb6f77285ca9e8
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-16 16:54:10 -0800

    build

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'http://www.samkuttenkuler.com'
+export const server ='http://localhost:3000'
