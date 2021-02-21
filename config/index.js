d1ce260c13cb9a719fceca1e4e5c7524afa59ef2
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-21 12:25:13 -0800

    nerw heroku app

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server =dev ? 'http://localhost:3000': 'https://stormy-everglades-47392.herokuapp.com'
+export const server = 'http://localhost:3000'
