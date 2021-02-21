f508a02608407caba48fac67455aca20b61b4859
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-21 14:49:29 -0800

    heroku build2

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = 'http://localhost:3000'
+export const server = dev ? 'http://localhost:3000' : 'https://6032e26fee96494d3690aa9e--hopeful-poincare-bad1e5.netlify.app'
