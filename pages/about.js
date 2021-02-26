0a55039e6a9aaf5bd81801672fecb299ac8ad34c
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-26 14:16:17 -0800

    spelling errors

--- pages/about.js
+++ pages/about.js
@@ -28,13 +28,13 @@ const About = ({}) => {
             
             <div className={styles.aboutContainer}>
                 <h3 className={styles.aboutText}>ABOUT</h3>
-                <p className={styles.bio}>&emsp; My name is Same Kuttenkuler and thank you for visiting my site. I am a Full Stack Engineer with a Bachelor’s of Science from Middle Tennesse State University.
+                <p className={styles.bio}>&emsp; My name is Sam Kuttenkuler and thank you for visiting my site. I am a Full Stack Engineer with a Bachelor’s of Science from Middle Tennesse State University.
                     I have spent the last 4 years designing and developing applications for the web and mobile mediums. In 2020, I completed course 
-                    work at the University of California, Berkely Bootcamp and recieved a certificate in Full Stack Web Development. 
-                    <br/>&emsp;Creative and curious. Diligent and meticulous. Previously a proffesional musician, I brought my all to delivering experiances. 
-                    I bring that same passion and drive for user experience with every product I ship. I am an adamant pursuer and lover of tailering memories and moments while aiming to leave lasting impressions with everyone that uses my apps.
+                    work at the University of California, Berkeley Bootcamp and received a certificate in Full Stack Web Development. 
+                    <br/>&emsp;Creative and curious. Diligent and meticulous. Previously a professional musician, I brought my all to delivering experiences. 
+                    I bring that same passion and drive for user experience with every product I ship. I am an adamant pursuer and lover of tailoring memories and moments while aiming to leave lasting impressions with everyone that uses my apps.
                     I pride myself on being a relentless problem solver and voracious learner. 
-                    Every product I build and ship is carefully planned, developed, and designed to give the user the exact experiance my client aims to give from the tech stack to the UX/UI.
+                    Every product I build and ship is carefully planned, developed, and designed to give the user the exact experience my client aims to give from the tech stack to the UX/UI.
                     I have been successful in this approach being a detail oriented programmer while remaining in a big picture mindset. If you have an idea for an application, please visit my contact page and let's build something great.       
                 <br/>
                 <br/>
